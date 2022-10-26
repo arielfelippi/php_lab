95ea70fbdca19bde2a7bfcd9c384a4d40ce85c15
root <root@bg-labX-pcX.localdomain>
2022-10-25 21:53:52 -0300

    conexao com banco e usuarios

---
+++ database/conexao.php
@@ -0,0 +1,24 @@
+<?php
+// Variaveis para conexao com o banco.
+$hostname = "localhost";
+$username = "aluno";
+$password = "qwe123";
+$database = "php_lab";
+$port = "3306";
+
+$connection = mysqli_connect(
+    $hostname,
+    $username,
+    $password,
+    $database,
+    $port);
+
+$dontConnectiuon = $connection == null || $connection == false || $connection == [];
+
+if ($dontConnectiuon) {
+    echo "deu ruim, nao conectou no banco.";
+    exit; // encerro o script do php.
+}
+
+// var_dump($connection);
+echo "Conexao realizada.";
